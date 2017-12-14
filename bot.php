6594b0ff23215cdce2d30ad9cb773a996adbd61e
FWSmith <fort2002@hotmail.com>
2017-12-15 05:27:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			$sender = $event['message']['sender'];
+			$sender = $event['message']['type'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
