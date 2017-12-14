c5b7baade045400930f4855709fdeeba6e7b62d4
FWSmith <fort2002@hotmail.com>
2017-12-15 05:49:41 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,16 +1,15 @@
 <?php
 $access_token = 'BdAQ3QuQX+ssTW55tgg2sJD911e0SN6/MmuTkXhxf16RTG3wqTibikzS0e2Vx0vCC3JqMNLSsenThtxSlG9dh2t8h/7OArNWet9tjYqAI/NgPc7TgIQwzJdk4VgUFJpirHRJgqdfL8v4QwsEaGiaBwdB04t89/1O/w1cDnyilFU=';
 
-//$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/bot/profile/U72c641a79b2f1a785a7b362df99931ae';
 
 $headers = array('Authorization: Bearer ' . $access_token);
-$file_get_content = file_get_contents('https://api.line.me/v2/bot/profile/U72c641a79b2f1a785a7b362df99931ae');
-//$ch = curl_init($url);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-//$result = curl_exec($ch);
-//curl_close($ch);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
 
-echo $file_get_content;
+echo $result;
 ?>
