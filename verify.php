7d10c21f23cccab2446b76dc3c69b93cd3e669c3
FWSmith <fort2002@hotmail.com>
2017-12-15 06:03:03 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -11,8 +11,8 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result->displayName;
+echo $result;
 ?>
 <script>
-   console.log(<?=$result?>);
+   console.log(<?=json_decode($result, true)?>);
 </script>
