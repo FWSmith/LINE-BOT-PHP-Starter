1a562cfbbb9ea4d39ed2e17d9ce3695f0d5eb5c7
FWSmith <fort2002@hotmail.com>
2017-12-15 10:32:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,10 +17,18 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			if($text == 'สวัสดี'){
+			  $messages = [
+			  'type' => 'text',
+			  'text' => 'สวัสดีครับ เจ้านาย'
+			  ];
+			}else{
+			  $messages = [
+			  'type' => 'text',
+			  'text' => 'ไม่รู้เรื่อง'
+			  ];	
+			}
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
