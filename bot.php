7bac8c10cd9506d72e49f5b8a9aad2c0854d2e0d
FWSmith <fort2002@hotmail.com>
2017-12-15 10:41:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if (!is_null($events['events'])) {
                            curl_setopt($ch_gp, CURLOPT_FOLLOWLOCATION, 1);
                            $result_gp = curl_exec($ch_gp);
                            curl_close($ch_gp);
-		           $result_decode = json_decode($result);
+		           $result_decode = json_decode($result_gp);
 			  $messages = [
 			    'type' => 'text',
 			    'text' => 'สวัสดีครับ เจ้านาย '.$result_decode->displayName
