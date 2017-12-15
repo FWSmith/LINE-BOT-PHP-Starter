379b6e0d817abe5283d6fbf6e5743a1953112869
FWSmith <fort2002@hotmail.com>
2017-12-15 11:10:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,9 +33,9 @@ if (!is_null($events['events'])) {
 		           $result_decode = json_decode($result_gp);
                            curl_close($ch_gp);
 		           $Name = $result_decode->displayName;
-		           if ($Name == "FORT") {
-			    $Display_Name = "โฟร์ท"
-			  }
+		           if (strpos($Name, 'FORT') !== false) {
+		             $Display_Name = 'โฟร์ท';
+		           }
 			  $messages = [
 			    'type' => 'text',
 			    'text' => 'สวัสดีครับ เจ้านาย '.$Display_Name
