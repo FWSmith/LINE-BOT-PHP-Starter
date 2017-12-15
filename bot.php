275af08d84fb8b05cb8bfce7016ae8bdc641129e
FWSmith <fort2002@hotmail.com>
2017-12-15 12:41:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -59,11 +59,41 @@ if (!is_null($events['events'])) {
 			  ];  
 			  }
 			  
-			}else{
-			  $messages = [
-			  'type' => 'text',
-			  'text' => 'ไม่รู้เรื่อง'
-			  ];	
+			}else if(strpos($text, 'ขอเบอร์') !== false || strpos($text, 'เบอร์') !== false){
+
+			    $messages = [
+			    {
+				  "type": "template",
+				  "altText": "this is a buttons template",
+				  "template": {
+				      "type": "buttons",
+				      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+				      "imageAspectRatio": "rectangle",
+				      "imageSize": "cover",
+				      "imageBackgroundColor": "#FFFFFF",
+				      "title": "Menu",
+				      "text": "Please select",
+				      "actions": [
+					  {
+					    "type": "postback",
+					    "label": "Buy",
+					    "data": "action=buy&itemid=123"
+					  },
+					  {
+					    "type": "postback",
+					    "label": "Add to cart",
+					    "data": "action=add&itemid=123"
+					  },
+					  {
+					    "type": "uri",
+					    "label": "View detail",
+					    "uri": "http://example.com/page/123"
+					  }
+				      ]
+				  }
+				}
+			    ];   
+			  
 			}
 			
 
