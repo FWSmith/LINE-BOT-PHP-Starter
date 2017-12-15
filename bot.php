c40b497c502cd284f65d9037e98609ccb4e234d4
FWSmith <fort2002@hotmail.com>
2017-12-15 21:44:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -145,5 +145,10 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
+$host = "fsmith.net";
+$user = "fsmithnet_root";
+$pass = "Chanan22lert";
+$dbname = "fsmithnet_license";
+$pdo = new PDO("mysql:dbname=$dbname;host=$host", $user, $pass);
 echo "OK";
 ?>
