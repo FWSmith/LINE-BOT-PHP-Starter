e60a4ed252427c710b22d80f1544a71970367c1c
FWSmith <fort2002@hotmail.com>
2017-12-16 22:28:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,43 +1,32 @@
 <?php
 $access_token = 'BdAQ3QuQX+ssTW55tgg2sJD911e0SN6/MmuTkXhxf16RTG3wqTibikzS0e2Vx0vCC3JqMNLSsenThtxSlG9dh2t8h/7OArNWet9tjYqAI/NgPc7TgIQwzJdk4VgUFJpirHRJgqdfL8v4QwsEaGiaBwdB04t89/1O/w1cDnyilFU=';
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+// Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-		 	$text = $event['message']['text'];				
-			//Get Group ID
-			if($event['source']['groupId'] != ''){
-			$groupId = $event['source']['groupId'];				
-			}
-			//Get User ID
-			$userId = $event['source']['userId'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-                        
-			// Build message to reply back
 
-			
-			if(strpos($text, 'สวัสดี') !== false || strpos($text, 'คนใช้') !== false){
-				$messages = [
-			    	[
-			    	'type' => 'text',
-			    	'text' => 'สวัสดีครับ'
-				]
-			     ];   
-			}
-		      
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages][0],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
