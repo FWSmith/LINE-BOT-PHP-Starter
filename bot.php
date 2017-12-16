edf134a318a5d59aa8785df49ffdeec97490de4f
FWSmith <fort2002@hotmail.com>
2017-12-16 11:49:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -364,6 +364,18 @@ if (!is_null($events['events'])) {
 				    "text"=>"ยังไม่มีเบอร์ครับ จุ้บๆ"
 			    ];
 			    }
+			}else if(strpos($text, 'ทดสอบ') !== false){
+			    $file = 'people.txt';
+				// The new person to add to the file
+				$person = "John Smith\n";
+				// Write the contents to the file, 
+				// using the FILE_APPEND flag to append the content to the end of the file
+				// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+				file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
+				$messages = [
+				    "type"=>"text", 
+				    "text"=>"ทำการสร้างไฟล์"
+			    ];
 			}else{
 			   
 			}
