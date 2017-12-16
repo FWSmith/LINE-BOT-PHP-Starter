9ceffd12948e3843b169bcc49801102fce4ae985
FWSmith <fort2002@hotmail.com>
2017-12-16 21:32:13 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
 $url = parse_url(getenv("mysql://b809e2f36f0522:01a9a1f5@us-cdbr-iron-east-05.cleardb.net/heroku_a0500905d74bead?reconnect=true"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-$conn = new mysqli($server, $username, $password, $db);
-
+$server = 'us-cdbr-iron-east-05.cleardb.net';
+$username = 'b809e2f36f0522';
+$password = '01a9a1f5';
+$db = 'heroku_a0500905d74bead';
+$mysqli = new mysqli($server, $username, $password, $db);
+if ($mysqli->connect_errno) {
+ echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+}
 ?>
