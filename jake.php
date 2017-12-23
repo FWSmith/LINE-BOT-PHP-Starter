62aa4af5e4f60d885b379ed303c83d1c86f614b8
FWSmith <fort2002@hotmail.com>
2017-12-23 14:47:10 +0700

    Update jake.php

--- jake.php
+++ jake.php
@@ -79,7 +79,7 @@ if (!is_null($events['events'])) {
 			           			]
 			           		];
 			           	}
-					}else if($Fetch_Train){
+					}else if($reply != ''){
 						$messages = [
 			           			[
 			           				'type' => 'text',
