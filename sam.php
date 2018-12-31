5bffe9815f2b7fdf80fc5eb9c6a67dfba2078b1c
FWSmith <fort2002@hotmail.com>
2018-12-31 19:35:03 +0700

    test

--- sam.php
+++ sam.php
@@ -1,5 +1,6 @@
 <?php
-   $accessToken = "mTOSBFPNNln49I6qmW55ctqvaHNfLGbKVQqfgbmQg1d0B5wEJFMbvW9ixkyuhX9aopgfZJBPdHap8BqPpPw4w1LFI+hMjlpj87VR/+aqK98XG7Bn8NMgayBGhTNC9vHeU25Me2+QvxM/dWV9eiBNdgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+if(isset($_POST)){
+ $accessToken = "mTOSBFPNNln49I6qmW55ctqvaHNfLGbKVQqfgbmQg1d0B5wEJFMbvW9ixkyuhX9aopgfZJBPdHap8BqPpPw4w1LFI+hMjlpj87VR/+aqK98XG7Bn8NMgayBGhTNC9vHeU25Me2+QvxM/dWV9eiBNdgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
    $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
    $arrayHeader = array();
@@ -8,18 +9,18 @@
    //รับข้อความจากผู้ใช้
    $message = $arrayJson['events'][0]['message']['text'];
    //รับ id ของผู้ใช้
-   $id = "U72c641a79b2f1a785a7b362df99931ae";
+   $id = ["U72c641a79b2f1a785a7b362df99931ae"];
    #ตัวอย่าง Message Type "Text + Sticker"
-      $arrayPostData['to'] = $id;
-      $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
-      $arrayPostData['messages'][1]['type'] = "sticker";
-      $arrayPostData['messages'][1]['packageId'] = "2";
-      $arrayPostData['messages'][1]['stickerId'] = "34";
-      pushMsg($arrayHeader,$arrayPostData);
+   $arrayPostData['to'] = $id;
+   $arrayPostData['messages'][0]['type'] = "text";
+   $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+   $arrayPostData['messages'][1]['type'] = "sticker";
+   $arrayPostData['messages'][1]['packageId'] = "2";
+   $arrayPostData['messages'][1]['stickerId'] = "34";
+   pushMsg($arrayHeader,$arrayPostData);
   
    function pushMsg($arrayHeader,$arrayPostData){
-      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $strUrl = "https://api.line.me/v2/bot/message/multicast";
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL,$strUrl);
       curl_setopt($ch, CURLOPT_HEADER, false);
@@ -31,5 +32,6 @@
       $result = curl_exec($ch);
       curl_close ($ch);
    }
-   exit;
+   exit;  
+}
 ?>
